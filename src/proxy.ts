95266a71c6fa17ccfb6c78c5455fabb99a169839
Saksham Galyan <146658307+Sakshamgalyan@users.noreply.github.com>
2026-01-01 21:57:53 +0530

    change whole app

--- src/proxy.ts
+++ src/proxy.ts
@@ -109,7 +109,8 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - public images (svg, png, jpg, jpeg, etc)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 };
